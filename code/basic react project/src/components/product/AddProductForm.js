0f8787ca03447704187cb3b5a4988640d428c09e
Sayed Azhar Sabri <afzal.sayed26@gmail.com>
2022-06-26 23:32:54 +0200

    Added useRef hook to get data from form

--- code/basic react project/src/components/product/AddProductForm.js
+++ code/basic react project/src/components/product/AddProductForm.js
@@ -1,26 +1,47 @@
+import { useRef } from 'react';
 import Styles from './AddProductForm.module.css';
 import CustomWrapper from '../layouts/CustomWrapper';
 function AddProductForm() {
+    const product_name_ref = useRef();
+    const image_ref = useRef();
+    const price_ref = useRef();
+    const description_ref = useRef();
+
+    function submitHandler(event) {
+        event.preventDefault();
+        const product_name = product_name_ref.current.value;
+        const image = image_ref.current.value;
+        const price = price_ref.current.value;
+        const description = description_ref.current.value;
+
+        const product = {
+            product_name,
+            image,
+            price,
+            description
+        }
+        console.log(product);
+    }
     return <CustomWrapper>
-        <form className={Styles.form}>
+        <form className={Styles.form} onSubmit={submitHandler}>
             <div className={Styles.groups}>
                 <label htmlFor="product_name">Product Name</label>
-                <input type="text" placeholder="" id="product_name" />
+                <input type="text" placeholder="" id="product_name" ref={product_name_ref} />
 
             </div>
             <div className={Styles.groups}>
                 <label htmlFor="image">Image</label>
-                <input type="text" placeholder="" id="image" />
+                <input type="text" placeholder="" id="image" ref={image_ref} />
 
             </div>
             <div className={Styles.groups}>
                 <label htmlFor="price">Price</label>
-                <input type="text" placeholder="" id="price" />
+                <input type="text" placeholder="" id="price" ref={price_ref} />
 
             </div>
             <div className={Styles.groups}>
                 <label htmlFor="description">Description</label>
-                <textarea rows="5" id="description" />
+                <textarea rows="5" id="description" ref={description_ref} />
 
             </div>
             <div className={Styles.btn}>
